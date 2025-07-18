6bf3e699753a6440523c86b0f8f62625d956b139
yyan0405 <yyan0405@student.monash.edu>
2025-07-18 15:27:06 +0800

    Week4_Validation

--- yyan-library/src/components/Form.vue
+++ yyan-library/src/components/Form.vue
@@ -7,32 +7,39 @@
           <div class="row mb-3">
             <div class="col-md-6">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" v-model="formData.username">
+              <input type="text" class="form-control" id="username" @blur="() => validateName(true)" @input="() => validateName(false)" v-model="formData.username">
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-md-6">
               <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password" v-model="formData.password">
+              <input type="password" class="form-control" id="password"
+              @blur="() => validatePassword(true)" @input="() => validatePassword(false)"
+              v-model="formData.password">
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
           <div class="row mb-3">
             <div class="col-md-6">
               <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian">
+                <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian" @change="validateResident">
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
+              <div v-if="errors.resident" class="text-danger">{{ errors.resident }}</div>
             </div>
             <div class="col-md-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender" v-model="formData.gender">
+              <select class="form-select" id="gender" v-model="formData.gender" @change="validateGender">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
             </div>
+            <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
-            <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+            <textarea class="form-control" id="reason" rows="3" v-model="formData.reason" @blur="() => validateReason(true)" @input="() => validateReason(false)"></textarea>
+            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -76,9 +83,105 @@ const formData = ref({
 const submittedCards = ref([]);
 
 const submitForm = () => {
-    submittedCards.value.push({
-        ...formData.value
-    });
+    validateName(true);
+    validatePassword(true);
+    validateResident();
+    validateGender();
+    validateReason(true);
+
+    const hasErrors = Object.values(errors.value).some(error => error !== null);
+
+    if (!hasErrors) {
+        submittedCards.value.push({
+            ...formData.value
+        });
+        clearForm();
+    }
+};
+const clearForm = () => {
+    formData.value = {
+        username: '',
+        password: '',
+        isAustralian: false,
+        reason: '',
+        gender: ''
+    };
+};
+const errors = ref({
+    username: null,
+    password: null,
+    resident: null,
+    reason: null,
+    gender: null
+});
+const validateName = (blur) => {
+    if (formData.value.username.length < 3) {
+        if(blur) {
+            errors.value.username = 'Username must be at least 3 characters';
+        }
+    } else {
+        errors.value.username = null;
+    }
+
+};
+const validatePassword = (blur) => {
+  const password = formData.value.password;
+  const minLength = 8;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`;
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = "Password must contain at least one uppercase letter.";
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = "Password must contain at least one lowercase letter.";
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = "Password must contain at least one number.";
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = "Password must contain at least one special character.";
+  } else {
+    errors.value.password = null;
+  }
+};
+const validateResident = () => {
+
+    if (!formData.value.isAustralian) {
+
+        errors.value.resident = null;
+    } else {
+        errors.value.resident = null;
+    }
+};
+
+const validateGender = () => {
+    if (!formData.value.gender || formData.value.gender === '') {
+        errors.value.gender = 'Please select a gender.';
+    } else {
+        errors.value.gender = null;
+    }
+};
+
+const validateReason = (blur) => {
+    const reason = formData.value.reason.trim();
+
+    if (reason.length === 0) {
+        if (blur) {
+            errors.value.reason = 'Please provide a reason for joining.';
+        }
+    } else if (reason.length < 10) {
+        if (blur) {
+            errors.value.reason = 'Reason must be at least 10 characters long.';
+        }
+    } else if (reason.length > 500) {
+        if (blur) {
+            errors.value.reason = 'Reason must be less than 500 characters.';
+        }
+    } else {
+        errors.value.reason = null;
+    }
 };
 </script>
 
